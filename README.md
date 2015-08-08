77ce22ed532f30cc95f8e14589f497da790cb4ef
Igor Rogatty <igor@wikia-inc.com>
2015-08-08 06:14:40 +0200

    Get data from API, get the diff, save the diff and new data to files

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 run from cron:
 
-get list from http://community.wikia.com/wikia.php?controller=CuratedContent&method=getCuratedContentQuality
-get list from db
-find what's added
-find what's removed
-save new list to db
-
-if there's a change send message to #x-wing
-add a screenshot maybe?
+* get list from http://community.wikia.com/wikia.php?controller=CuratedContent&method=getCuratedContentQuality
+* get old list from json file
+* find what's added
+* find what's removed
+* if there's a change:
+  * save new list to json file
+  * send message to #x-wing
+  * add a screenshot maybe?
+  * save changelog to json file
--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 'use strict';
 
-var db = require('./lib/db');
+var jsonfile = require('jsonfile'),
+	request = require('request'),
+	diff = require('./lib/diff'),
+	apiUrl = 'http://community.wikia.com/wikia.php?controller=CuratedContent&method=getCuratedContentQuality',
+	dataDir = 'data/',
+	saveFile = function (fileName, content) {
+		jsonfile.writeFile(dataDir + fileName + '.json', content, function (err) {
+			if (err) {
+				console.error('There was error when writing to file', err);
+			}
+		});
+	};
 
 try {
 	//require('./setEnv')();
 } catch (e) {}
+
+request(apiUrl, function (error, response, body) {
+	var curatedContentQuality,
+		curatedContentQualityPerWiki,
+		currentState,
+		previousState,
+		difference,
+		now = new Date(),
+		nowString = '' + now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+
+	if (!error && response.statusCode == 200) {
+		previousState = jsonfile.readFileSync(dataDir + 'current.json', {
+			throws: false
+		});
+
+		curatedContentQuality = JSON.parse(body);
+		curatedContentQualityPerWiki = curatedContentQuality.curatedContentQualityPerWiki;
+
+		if (curatedContentQualityPerWiki) {
+			currentState = Object.keys(curatedContentQualityPerWiki);
+		}
+
+		if (previousState && currentState) {
+			difference = diff(previousState, currentState);
+			if (difference.added.length || difference.removed.length) {
+				saveFile(nowString, difference);
+			}
+		}
+
+		if (currentState) {
+			saveFile('current', currentState);
+		}
+	} else {
+		console.error('Error when trying to connect to ' + apiUrl, error, response);
+	}
+});
 
 
